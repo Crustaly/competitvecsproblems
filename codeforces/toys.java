5df9a91591514f7dc6136ee7d0d38c07643585bd
Crystal Yang <106780309+Crustaly@users.noreply.github.com>
2023-06-12 20:53:33 -0500

    Add files via upload

---
+++ codeforces/toys.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class toys {
+	static ArrayList<Integer> toys;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int ts = sc.nextInt();
+		for(int t=0;t<ts;t++) {
+			int n =  sc.nextInt();
+			toys = new ArrayList<Integer>();
+			for(int i =0;i<n;i++) {
+				toys.add(i);
+			}
+			Collections.sort(toys);
+			int l=0;
+			int h = toys.get(n-1);
+			while(l<h) {
+				int mid = (l+h)/2;
+				if(works(mid)) {
+					h = mid;
+				}
+				else {
+					l=mid+1;
+				}
+			}
+			System.out.println(l);
+		}
+	}
+	static boolean works(int mid) {
+		long range = 2*mid;
+		int start =-1;
+		int needed =1;
+		for(int i =0;i<toys.size();i++) {
+			if(start == -1) {
+				start = toys.get(i);
+			}
+			if(toys.get(i)-start>range) {
+				start = toys.get(i);
+				needed++;
+			}
+		}
+		return needed<=3;
+	}
+}
